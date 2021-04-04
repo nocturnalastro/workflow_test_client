0fa9a9713c099f370baede356fe20d60bca02910
Michele Costa <mtcosta@protonmail.com>
2021-04-04 20:34:40 +0100

    Improve stack and add sparse stack to possibily reduce context size

--- src/stack.py
+++ src/stack.py
@@ -1,12 +1,13 @@
 from copy import deepcopy
+from collections import MutableMapping
 from .exceptions import InvalidEmptyStackOperation
-from .utils import deepmerge
+from .utils import deepmerge, deepdiff
 
 
 class Stack:
     def __init__(self, head, tail=None, merge_strat=deepmerge):
         self.head = head
-        self.tail = EmptyStack() if tail is None else tail
+        self._tail = EmptyStack() if tail is None else tail
         self._merge_strat = merge_strat
 
     def push(self, item):
@@ -17,7 +18,7 @@ class Stack:
         return self
 
     def pop(self):
-        return self.tail
+        return self._tail
 
 
 class EmptyStack:
@@ -60,3 +61,54 @@ class VirtualStack:
 
     def push_head_copy(self):
         self.push(self.get_head())
+
+
+class SparseStack:
+    """The sparse stack is specific for the context use case where
+    each layer is a variation on the optional than a completely
+    new object. This allows the sparse stack only to store the changes between
+    each layer.
+
+    This requires one of two things either a flattening of the stack
+    before returning the head or a facade which acts like the original data type
+    to be which can do the flattening-like operations in real-time returned instead.
+    This quickly becomes complex with nested data the flatten and return approach is
+    used here
+
+    Note: the assumed type with default merge_strat and diff_strat is dicts if you wish
+    to use a different type you must pass your own merge and diff strats
+    """
+
+    def __init__(
+        self,
+        head,
+        tail=None,
+        merge_strat=deepmerge,
+        diff_strat=deepdiff,
+    ):
+        self.merge_strat = merge_strat
+        self.diff_strat = diff_strat
+        self._head = diff_strat(head, tail)
+        self._tail = EmptyStack() if tail is None else tail
+
+    @property
+    def head(self):
+        res = self._head
+        if not isinstance(self._tail, EmptyStack):
+            res = self.merge_strat(self._tail.head, res)
+        return deepcopy(res)
+
+    def update(self, update):
+        self._head = self.merge_strat(self._head, update)
+        return self
+
+    def push(self, item):
+        return self.__class__(
+            item,
+            self,
+            merge_strat=self.merge_strat,
+            diff_strat=self.diff_strat,
+        )
+
+    def pop(self):
+        return self._tail
--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,14 @@
-def deepmerge(*dicts):
+def deepmerge(*dicts: dict) -> dict:
+    """Returns a merge of the nested dicts with the
+    later dict overwritting the newer
+    ```
+    >>> A = {"x": 1, "z": 4}
+    >>> A = {"x": 2, "y": 3}
+    >>> merge_stat(A, B)
+    {"x": 2, "y": 3, "z": 4}
+
+
+    """
     result = dicts[0]
     for d in dicts[1:]:
         for key, value in d.items():
@@ -7,4 +17,27 @@ def deepmerge(*dicts):
                 result[key] = deepmerge(current, value)
             else:
                 result[key] = value
+    return result
+
+
+def deepdiff(a: dict, b: dict) -> dict:
+    """
+    returns new values and updates to existing values in A relative to B
+    and values which are present in B but not in A are dropped e.g.:
+    ```
+    >>> A = {"x": 1, "y": 3, "z": 4}
+    >>> B = {"x": 2, "y": 3, "w": 5}
+    >>> diff_strat(A, B)
+    {"x": 1, "z": 4}
+    ```
+    """
+    result = {}
+    for key, value_a in a.items():
+        if key not in b:
+            result[key] = value_a
+        elif isinstance(value_a, dict) and isinstance(value_b := b[key], dict):
+            if inner_diff := deepdiff(value_a, value_b):
+                result[key] = inner_diff
+        elif value_a != b[key]:
+            result[key] = value_a
     return result
