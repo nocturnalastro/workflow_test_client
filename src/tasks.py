3f133c68feab5490a7a8d49e3ffbc9075c42e61b
Michele Costa <mtcosta@protonmail.com>
2021-04-04 20:36:07 +0100

    Added more tasks and made setting result in the context more robust

--- src/tasks.py
+++ src/tasks.py
@@ -1,8 +1,12 @@
 from copy import deepcopy
-from .registry import TASK_TYPES
+
 from . import utils
-from .components import Component, COMPONENTS
+from .components import COMPONENTS, Component
 from .path import evaluator
+from .registry import TASK_TYPES
+from .templating import process_template
+from .validators import Validator
+from .context import ExecutionContext
 
 jsonpath = evaluator()
 
@@ -14,14 +18,30 @@ __all__ = (
 
 class Task:
     _requires_input = False
+    _complete_by_default = True
 
-    def __init__(self, execution_context, task):
+    def __init__(self, execution_context: ExecutionContext, task: dict):
         self._task = task
         self._execution_context = execution_context
-        self._complete = False
+        self._complete = self._complete_by_default
         self._exit_reason = "success"
         if execution_context is not None:
             execution_context.register_task(self)
+        self.preconditions: list[Validator] = (
+            [self._process_validator(p) for p in task["preconditions"]]
+            if task.get("preconditions")
+            else []
+        )
+
+    def _process_validator(self, validator: str):
+        return Validator(
+            validator_name=validator, execution_context=self._execution_context
+        )
+
+    def run(self):
+        if not self._requires_input and self._complete:
+            self.publish_result()
+        return self
 
     @property
     def name(self):
@@ -34,9 +54,14 @@ class Task:
     def publish_result(self):
         self._execution_context.update_result(self.result)
 
+    def set_as_complete(self):
+        self.publish_result()
+        self._complete = True
+
 
 class Screen(Task):
     _requires_input = True
+    _complete_by_default = False
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -119,6 +144,7 @@ class Screen(Task):
 
 class JsonRpc(Task):
     _requires_input = True
+    _complete_by_default = False
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
@@ -163,6 +189,7 @@ class JsonRpc(Task):
             path=self._task["destination_path"],
             value=result,
         )
+        self.set_as_complete()
 
     @property
     def result(self):
@@ -172,24 +199,40 @@ class JsonRpc(Task):
 class Update(Task):
     task_type = "update"
 
+    def _process_instruction(self, instruction, extra_context=None):
+        context = self._execution_context.state
+        if extra_context:
+            context = context | extra_context
+        if "value" in instruction:
+            value = instruction["value"]
+        elif "key" in instruction:
+            value = jsonpath.get_one(context=context, path=instruction["key"])
+        elif "template" in instruction:
+            value = process_template(instruction["template"], context=context)
+        else:
+            raise KeyError("Value or key not in instruction")
 
-class Redirect(Task):
-    task_type = "redirect"
+        return jsonpath.set(context={}, path=instruction["result_key"], value=value)
 
-
-class Condition:
-    task_type = "condition"
-
-
-class DomainParam:
-    task_type = "domain_param"
+    @property
+    def result(self):
+        res = {}
+        for instruction in self._task["tasks"]:
+            res = utils.deepmerge(res, self._process_instruction(instruction, res))
+        return res
 
 
 class Flow(Task):
+    _complete_by_default = False
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self._task |= self._execution_context.repos.flows[self._task["name"]]
         self._task_iter = None
+        self._actions = []
+        self._task_names = [
+            f"{self._task['name']}.{t['name']}" for t in self._task["tasks"]
+        ]
 
     def _get_task_instance(self, task, execution_context):
         return TASK_TYPES[task["type"]](task=task, execution_context=execution_context)
@@ -227,37 +270,121 @@ class Flow(Task):
                 if inst.name in interupt_tasks:
                     yield inst
                 while inst.requires_input:
-                    yield inst
+                    yield inst.run()
 
                 if (
                     isinstance(inst, TASK_TYPES["event"])
                     and inst._task["action"] == "break"
                 ):
+                    self._actions.append("break")
                     break
 
+            # Add task result to flow context
             self._execution_context.update_state(context.result)
-        self._complete = True
+        self.set_as_complete()
 
     def get_task(self, interupt_tasks=None):
         if self._task_iter is None:
             self._task_iter = self._input_task_iter(interupt_tasks)
         return next(self._task_iter)
 
+    def get_task_names(self):
+        return self._task_names
+
 
 class WhileLoop(Flow):
-    pass
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._loop_config = self._task["config"]
+        self._conditions = [
+            self._process_validator(p) for p in self._loop_config["conditions"]
+        ]
+        self._result = []
+
+    @property
+    def result(self):
+        return jsonpath.set(
+            context={},
+            path=self._loop_config["destination_path"],
+            value=self.self._result,
+        )
+
+    def _input_task_iter(self, interupt_tasks=None):
+        while all(c.validate() for c in self._conditions):
+            yield from super()._input_task_iter(interupt_tasks)
+            if "break" in self._actions:
+                break
+            # super()._input_task_iter will mark its self as complete
+            self._complete = False
+            # Take snapshot of context for after iteration
+            self._result.append(super().result)
+        self.set_as_complete()
 
 
 class ForLoop(Flow):
-    pass
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._loop_config = self._task["config"]
+        self._result = []
 
+    @property
+    def result(self):
+        return jsonpath.set(
+            context={},
+            path=self._loop_config["destination_path"],
+            value=self.self._result,
+        )
 
-class ClearDomainParams(Task):
-    pass
+    def _get_loop_values(self):
+        return jsonpath.get_one(
+            context=self._execution_context.state,
+            path=self._loop_config["iterable_path"],
+        )
+
+    def _input_task_iter(self, interupt_tasks=None):
+        for loop_context in self._get_loop_values():
+            self._execution_context.update_state(loop_context)
+            yield from super()._input_task_iter(interupt_tasks)
+            if "break" in self._actions:
+                break
+            # super()._input_task_iter will mark its self as complete
+            self._complete = False
+            # Take snapshot of context for after iteration
+            self._result.append(self._execution_context.result)
+        self.set_as_complete()
 
 
 class Event(Task):
     pass
+
+
+class Redirect(Task):
+    # By the fact im not sure how this should behave
+    # means I think it shouldn't be in the core
+    # perhaps in a http addons module?
+    # the addon could inject redirect into
+    # the namespace of the core stuff like a
+    # plugin
+    task_type = "redirect"
+
+
+class Condition:
+    # This requires some thinking about as it does stack
+    # manipulation perhaps the result should be a set of
+    # insructions of which stack and task to jump to?
+    task_type = "condition"
+
+
+class DomainParam:
+    # This is another browser specific task
+    # not sure how to handle this either
+    task_type = "domain_param"
+
+
+class ClearDomainParams(Task):
+    # This is another browser specific task
+    # not sure how to handle this either
+    task_type = "clear_domain_params"
 
 
 TASK_TYPES.update(
@@ -268,10 +395,11 @@ TASK_TYPES.update(
         "while_loop": WhileLoop,
         "for_loop": ForLoop,
         "update": Update,
-        "redirect": Redirect,
-        "condition": Condition,
-        "domain_params": DomainParam,
-        "clear_domain_params": ClearDomainParams,
+        # "condition": Condition,
         "event": Event,
+        # ---------------#
+        # "redirect": Redirect,
+        # "domain_params": DomainParam,
+        # "clear_domain_params": ClearDomainParams,
     }
 )
