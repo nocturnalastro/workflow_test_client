e07a1129d2a1d6da1dc29e81777044113b54b86e
Michele Costa <mtcosta@protonmail.com>
2021-04-05 18:50:14 +0100

    Make deepmerge and deepdiff less likely to cause state coruption

--- src/utils.py
+++ src/utils.py
@@ -1,3 +1,18 @@
+from copy import deepcopy
+
+
+def _deepmerge(*dicts: dict) -> dict:
+    result = dicts[0]
+    for d in dicts[1:]:
+        for key, value in d.items():
+            current = result.get(key, None)
+            if isinstance(current, dict):
+                result[key] = _deepmerge(current, value)
+            else:
+                result[key] = value
+    return result
+
+
 def deepmerge(*dicts: dict) -> dict:
     """Returns a merge of the nested dicts with the
     later dict overwritting the newer
@@ -9,14 +24,19 @@ def deepmerge(*dicts: dict) -> dict:
 
 
     """
-    result = dicts[0]
-    for d in dicts[1:]:
-        for key, value in d.items():
-            current = result.get(key, None)
-            if isinstance(current, dict):
-                result[key] = deepmerge(current, value)
-            else:
-                result[key] = value
+    return _deepmerge(*map(deepcopy, dicts))
+
+
+def _deepdiff(a: dict, b: dict) -> dict:
+    result = {}
+    for key, value_a in a.items():
+        if key not in b:
+            result[key] = value_a
+        elif isinstance(value_a, dict) and isinstance(value_b := b[key], dict):
+            if inner_diff := _deepdiff(value_a, value_b):
+                result[key] = inner_diff
+        elif value_a != b[key]:
+            result[key] = value_a
     return result
 
 
@@ -31,13 +51,4 @@ def deepdiff(a: dict, b: dict) -> dict:
     {"x": 1, "z": 4}
     ```
     """
-    result = {}
-    for key, value_a in a.items():
-        if key not in b:
-            result[key] = value_a
-        elif isinstance(value_a, dict) and isinstance(value_b := b[key], dict):
-            if inner_diff := deepdiff(value_a, value_b):
-                result[key] = inner_diff
-        elif value_a != b[key]:
-            result[key] = value_a
-    return result
+    return _deepmerge(deepcopy(a), deepcopy(b))
